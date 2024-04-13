60f887b96cf98a2361b4399a145f1a914ed9d438
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-04-13 16:15:44 +0700

    fix: change authorize admin only for delete Report

--- Backend/routes/reports.js
+++ Backend/routes/reports.js
@@ -9,7 +9,7 @@ const { protect , authorize } = require('../middleware/auth');
 //Re-route into other resource routers
 
 router.route('/').get(protect , authorize('user'),getReports).post(protect , authorize('user'), createReport) ;
-router.route('/:id').get(protect , authorize('user'), getReport).put(protect , authorize('user'), deleteReport);
+router.route('/:id').get(protect , authorize('user'), getReport).put(protect , authorize('user'), updateReport).delete(protect , authorize('admin'), deleteReport);
 
 module.exports=router ;
 
