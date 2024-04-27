1911dd95e646b17fc231900f3b22a97f87890173
Siratee Topradit <tae.siratee@gmail.com>
2024-04-27 22:27:42 +0700

    Refactor Dentist page and DentistCatalog component

--- Frontend/src/app/(dentistinfo)/dentist/page.tsx
+++ Frontend/src/app/(dentistinfo)/dentist/page.tsx
@@ -6,20 +6,27 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function Dentist() {
+    const dentist = getDentists();
 
-  const dentist = getDentists();
-
-  return (
-    <main className="text-center p-5 ">
-        <h1 className="text-center font-semibold text-4xl mb-10 my-5">Our Dentists</h1>
-        <Suspense fallback={<div>
-          <p className="mt-20 mb-5 text-black text-center text-5xl text-bold space-y-6">Loading... </p>
-          <div className=" mb-20 "><LinearProgress/></div>
-        </div>}>
-         
-          <DentistCatalog dentistsJson={dentist}/>
-          
-        </Suspense>
-    </main>
-  );
+    return (
+        <main className="text-center p-5 ">
+            <h1 className="text-center font-semibold text-4xl mb-10 my-5">
+                Our Dentists
+            </h1>
+            <Suspense
+                fallback={
+                    <div>
+                        <p className="mt-20 mb-5 text-black text-center text-5xl text-bold space-y-6">
+                            Loading...{" "}
+                        </p>
+                        <div className=" mb-20 ">
+                            <LinearProgress />
+                        </div>
+                    </div>
+                }
+            >
+                <DentistCatalog dentistsJson={dentist} />
+            </Suspense>
+        </main>
+    );
 }
