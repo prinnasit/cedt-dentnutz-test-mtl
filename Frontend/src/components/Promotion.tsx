0e0ea7e774cfd47e00e0225c87fb922d430741e4
PePeKirika <nongpepe@gmail.com>
2024-04-28 14:27:34 +0700

    fix no promotion for dentist

--- Frontend/src/components/Promotion.tsx
+++ Frontend/src/components/Promotion.tsx
@@ -7,27 +7,34 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import DentistCatalogHorizontal from "./DentistCatalogHorizontal";
 
 export default async function Promotion() {
+    const session = await getServerSession(authOptions);
+    if (!session) return null;
     const dentist = getDentists();
-
-    return (
-        <main className="p-5">
-            <h1 className="text-center font-semibold text-4xl text-black">
-                Our Dentists
-            </h1>
-            <Suspense
-                fallback={
-                    <div>
-                        <p className="mt-20 mb-5 text-black text-center text-5xl text-bold space-y-6">
-                            Loading...{" "}
-                        </p>
-                        <div className=" mb-20 ">
-                            <LinearProgress />
+    
+    if (session.user.type !== "dentist") {
+        return (
+            <main className="p-5">
+                <h1 className="text-center font-semibold text-4xl text-black">
+                    Our Dentists
+                </h1>
+                <Suspense
+                    fallback={
+                        <div>
+                            <p className="mt-20 mb-5 text-black text-center text-5xl text-bold space-y-6">
+                                Loading...{" "}
+                            </p>
+                            <div className=" mb-20 ">
+                                <LinearProgress />
+                            </div>
                         </div>
-                    </div>
-                }
-            >
-                <DentistCatalogHorizontal dentistsJson={dentist} />
-            </Suspense>
-        </main>
-    );
+                    }
+                >
+                    <DentistCatalogHorizontal dentistsJson={dentist} />
+                </Suspense>
+            </main>
+        );
+    } else {
+        return null
+    }
+    
 }
