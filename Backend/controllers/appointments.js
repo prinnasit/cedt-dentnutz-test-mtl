9afce91aac5c54f628cf11db1edfe2110c14c561
PePeKirika <nongpepe@gmail.com>
2024-04-29 22:24:54 +0700

    fix finish past appointment

--- Backend/controllers/appointments.js
+++ Backend/controllers/appointments.js
@@ -166,7 +166,7 @@ exports.updateAppointment = async (req, res, next) => {
     const currentDate = new Date();
     let appointment = await Appointment.findById(req.params.id).populate('user dentist');
     let timeBeforeUpdate = appointment.appDate;
-    if(currentDate > appointment.appDate){
+    if(currentDate > appointment.appDate && !req.body.finished){
       return res.status(400).json({
         success: false,
         message: 'This appointment is in progress',
