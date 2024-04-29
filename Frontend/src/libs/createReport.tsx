fb3d37e6ef2440b375c17b18fb5c36cba4f0b2c6
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-04-29 15:28:02 +0700

    fix: change create report methode

--- Frontend/src/libs/createReport.tsx
+++ Frontend/src/libs/createReport.tsx
@@ -1,5 +1,5 @@
 
-export default async function createReport(patientID:string,dentistID:string, apptID:string, apptDate:Date, treatment:string, presMed:string ,rec:string, token:string) {
+export default async function createReport(apptID:string, treatment:string, presMed:string ,rec:string, token:string) {
 
     const response = await fetch('https://cedt-se-project-dentnutz-backend.vercel.app/api/v1/reports', {
         method: 'POST',
@@ -7,7 +7,7 @@ export default async function createReport(patientID:string,dentistID:string, ap
             'authorization': `Bearer ${token}`,
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({patientId: patientID, appointmentId: apptID, dentistId: dentistID, treatment: treatment, prescribed_medication: presMed, recommendations: rec, date: apptDate})
+        body: JSON.stringify({appointmentId: apptID, treatment: treatment, prescribed_medication: presMed, recommendations: rec})
     })
     const res = await response.json()
     if (!response.ok) {
