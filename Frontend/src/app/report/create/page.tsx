88315885a7b89116613853a32f6eed26032c3490
prinnasit <prinnasit@gmail.com>
2024-04-29 17:43:38 +0700

    stly update report and create report

--- Frontend/src/app/report/create/page.tsx
+++ Frontend/src/app/report/create/page.tsx
@@ -71,28 +71,33 @@ export default function addReport() {
       };
   
   return (
-    <main className="justify-center items-center p-5 flex flex-col">
-        <h1 className="text-center font-semibold text-4xl mb-10 mt-5">Create Report</h1>
-        <div className="border-2 border-gray-300 rounded-2xl w-4/5 flex flex-col justify-items-center content-center p-5 space-y-2">
-            
-            
-            <div className="flex flex-row  mt-15 mb-15">
-                <div className="text-xl basis-1/4 text-black">Treatment :</div>
-                <TextField multiline className="basis-3/4" id="outlined-basic" label="Treatment" variant="outlined" 
-                onChange={(e)=>{setTreatment(e.target.value)}}/>
-            </div>
-            <div className="flex flex-row ">
-                <div className="text-xl basis-1/4 text-black">Medication :</div>
-                <TextField multiline className="basis-3/4" id="outlined-basic" label="Medication" variant="outlined" 
-                onChange={(e)=>{setMedication(e.target.value)}}/>
-            </div>
-            <div className="flex flex-row ">
-                <div className="text-xl basis-1/4 text-black">Recommmendation : </div>
-                <TextField multiline className="basis-3/4 " id="outlined-basic" label="Recommmendation" variant="outlined" 
-                onChange={(e)=>{setRecommendation(e.target.value)}}/>
-            </div>
+    <main className="flex justify-center items-center p-10 flex-col mb-10 ">
+    <h1 className="mx-auto w-fit text-4xl mb-10 font-bold text-black mt-7 ">Create Report</h1>
+    <div className="shadow-lg rounded-lg w-4/5 flex flex-col p-5 space-y-7 text-center  ">
+        
+    <div className="items-center ">
+        <div className="w-1/4 text-black text-s ml-10  mb-1">Treatment</div>
+        <input type="text" className="rounded-2xl text-black outline-2 border border-gray-300 hover:border-gray-500 focus:border-bule-500 focus:outline-none focus:ring-2 
+        focus:ring-blue-500 focus:ring-opacity-70 w-3/4 p-3"
+        onChange={(e) => {setTreatment(e.target.value)}}  value={treatment}/>
+   
+    </div>
+    <div className=" items-center ">
+        <div className="w-1/4 text-black text-s ml-10  mb-1">Medication</div>
+        <input type="text" className="rounded-2xl text-black outline-2 border border-gray-300 hover:border-gray-500 focus:border-bule-500 focus:outline-none focus:ring-2 
+        focus:ring-blue-500 focus:ring-opacity-70 w-3/4 p-3"
+        onChange={(e) => {setMedication(e.target.value)}}  value={medication}/>
+        
+    </div>
+    <div className=" items-center mb-5">
+        <div className="w-1/4 text-black text-s ml-[70px] ">Recommendation:</div>
+      
+        <textarea className="rounded-2xl text-black outline-2 border border-gray-300 hover:border-gray-500 focus:border-bule-500 focus:outline-none focus:ring-2 
+        focus:ring-blue-500 focus:ring-opacity-70 w-3/4 p-2" rows={4}
+        onChange={(e) => {setRecommendation(e.target.value)}}  value={recommendation}></textarea>
+    </div>
 
-            <button className="block bg-orange-400 rounded-full hover:bg-orange-300 text-white font-semibold px-5 py-3 shadow-xl text-white mx-auto text-2xl"
+            <button className="bg-orange-400 rounded-full hover:bg-orange-300 text-white font-semibold px-8 py-1 shadow-xl text-white mx-auto text-xl "
                 onClick={makingReport} disabled={isClickedRef.current}
             >Create Report</button>
         </div>
