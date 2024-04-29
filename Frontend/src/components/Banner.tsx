065398db38f87a2e12d9e7eb909d30dc4a2616ae
Siratee Topradit <tae.siratee@gmail.com>
2024-04-29 21:10:11 +0700

    Refactor Banner component styles and update GitHub link color

--- Frontend/src/components/Banner.tsx
+++ Frontend/src/components/Banner.tsx
@@ -53,11 +53,11 @@ export default function Banner() {
                 />
             </div>
             <main className="grid lg:grid-cols-2 place-items-center pt-16 pb-8 md:pt-12 md:pb-24 z-30">
-                <div className="p-12 bg-white-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-40 border border-gray-100">
+                <div className="p-12 bg-white rounded-lg">
                     <h1 className="text-5xl lg:text-6xl xl:text-7xl font-bold lg:tracking-tight xl:tracking-tighter text-black">
                         Dentnutz
                     </h1>
-                    <h2 className="text-3xl text-white lg:text-5xl xl:text-6xl font-bold lg:tracking-tight xl:tracking-tighter ">
+                    <h2 className="text-3xl text-sky-500 lg:text-5xl xl:text-6xl font-bold lg:tracking-tight xl:tracking-tighter ">
                         Teeth Protector
                     </h2>
                     {session && me ? (
@@ -97,29 +97,12 @@ export default function Banner() {
                     <div className="mt-6 flex flex-col sm:flex-row gap-3">
                         <a
                             onClick={(e)=>{e.stopPropagation(); router.push(path);}}
-                            className="rounded-xl bg-orange-400 text-white text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-5 py-2.5 hover:bg-gray-800  border-2 border-transparent flex gap-1 items-center justify-center"
+                            className="rounded-xl bg-orange-400 text-white text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-5 py-2.5 hover:bg-orange-800  border-2 border-transparent flex gap-1 items-center justify-center"
                         >
                           {buttonMessage}
                         </a>
                         
-                        <a
-                            href="https://github.com/2110503TACEDT66/cedt-se-project-dentnutz"
-                            className="rounded-xl text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-5 py-2.5 bg-white border-2 border-black hover:bg-gray-100 text-black  flex gap-1 items-center justify-center"
-                        >
-                            <svg
-                                viewBox="0 0 24 24"
-                                className="text-black w-4 h-4"
-                                astro-icon="bx:bxl-github"
-                            >
-                                <path
-                                    fill="currentColor"
-                                    fill-rule="evenodd"
-                                    d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"
-                                    clip-rule="evenodd"
-                                ></path>
-                            </svg>
-                            GitHub Repo
-                        </a>
+                        
                     </div>
                 </div>
             </main>
