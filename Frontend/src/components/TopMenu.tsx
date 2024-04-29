df0a0ace2b37ca75201a125cbfeaf2660c90b047
Siratee Topradit <tae.siratee@gmail.com>
2024-04-29 21:10:25 +0700

    Update TopMenu component styles

--- Frontend/src/components/TopMenu.tsx
+++ Frontend/src/components/TopMenu.tsx
@@ -124,7 +124,7 @@ export default function TopMenu() {
             <Disclosure as="nav" className="">
                 {({ open }) => (
                     <>
-                        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                        <div className="px-2 sm:px-6 lg:px-8">
                             <div className="relative flex h-14 items-center justify-between">
                                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                     <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
