546e40638fcd12b5da57ccafd47c70224aa5fce1
prinnasit <prinnasit@gmail.com>
2024-07-01 03:24:35 +0700

    booking (view)

--- testcase/tests/Basetset.ts
+++ testcase/tests/Basetset.ts
@@ -20,6 +20,11 @@ export class BaseTest {
         await this.page.getByRole('link', { name: 'Booking' }).click();
     }
 
+    async navigateToappointment() {
+        await this.page.goto('https://frontendsw-mtl.vercel.app/');
+        await this.page.getByRole('link', { name: 'Appointment' }).click();
+    }
+
     async login(email, password) {
         await this.page.getByPlaceholder('email').fill(email);
         await this.page.getByLabel('Password').fill(password);
@@ -46,7 +51,7 @@ export class BaseTest {
     //can you meake time parameter have only two value "morning" or "afternoon"
     
     
-    async filldatabooking( doctername , time : "morning" | "afternoon") {
+    async filldatabooking( doctername , time : "morning" | "afternoon" | null , day , month , year , fillwithkeyborad : boolean) {
         let timeValue;
         if (time === "afternoon") {
            timeValue = "15.00";
@@ -54,19 +59,40 @@ export class BaseTest {
         else if(time === "morning") {
            timeValue = "09.00";
         }
+        else if(time === null){
+            timeValue = null;
+        }
         else{
             throw new Error("time must be morning or afternoon")
         }
 
-
-        await this.page.getByLabel('dentist').click();
-        await this.page.getByRole('option', { name: doctername }).click();
-        await this.page.getByLabel('Choose date').click();
-        await this.page.getByLabel('calendar view is open, switch').click();
-        await this.page.getByRole('button', { name: '2025' }).click();
-        await this.page.getByRole('gridcell', { name: '27' }).click();
-        await this.page.getByLabel('Time').click();
-        await this.page.getByRole('option', { name: timeValue }).click();
+        if(doctername !== null){
+            await this.page.getByLabel('dentist').click();
+            await this.page.getByRole('option', { name: doctername }).click();
+        }
+        
+        if(!fillwithkeyborad){
+            await this.page.getByLabel('Choose date').click();
+            await this.page.getByLabel('calendar view is open, switch').click();
+            await this.page.getByRole('button', { name: year }).click();
+            for (let i = 1; i < month; i++) {
+                await this.page.getByRole('button', { name: 'Next month' }).click();
+            }
+            const dayLocator = `button[role="gridcell"]:has-text("${day}")`;
+            await this.page.locator(dayLocator).first().click();
+        }
+        else{ 
+            if(month !== null){
+                const strmonth = month.toString();
+            }
+            await this.page.getByPlaceholder('mm/dd/yyyy').fill(day + '/' + month + '/' + year);
+        }
+        
+        if(time !== null){
+            await this.page.getByLabel('Time').click();
+            await this.page.getByRole('option', { name: timeValue }).click();
+        }
+       
         await this.page.getByRole('button', { name: 'Create Appointment' }).click();
 
     }
@@ -82,26 +108,67 @@ export class BaseTest {
         await expect(this.page.getByRole('button', { name: 'Create Appointment' })).toBeVisible
         await this.page.getByLabel('Choose date').click();
         await expect(this.page.locator('//div[@role="dialog"]/div/div')).toBeVisible();
-        await expect(this.page.getByText('June 2024SMTWTFS123456789101112131415161718192021222324252627282930')).toBeVisible()
+        await expect(this.page.locator('//div[@class="MuiPickersFadeTransitionGroup-root css-1bx5ylf"]')).toBeVisible();
         await expect(this.page.getByLabel('calendar view is open, switch')).toBeVisible();
         await expect(this.page.locator('.MuiPickersArrowSwitcher-root')).toBeVisible();
     }
 
-    async VerifyIncompleteBooking() {
+    async VerifyIncompleteBooking(Incomplete : "date" | "time" | "dentist" | "all") {
         await expect(this.page.locator('div').filter({ hasText: '!' }).nth(2)).toBeVisible();
         await expect(this.page.getByLabel('Incomplete')).toBeVisible();
         await expect(this.page.getByRole('heading', { name: 'Incomplete' })).toBeVisible();
-        await expect(this.page.getByText('Please select date for')).toBeVisible();
+        if(Incomplete === "date"){
+            await expect(this.page.getByText('Please select date for appointment')).toBeVisible();
+        }
+        else if(Incomplete === "time"){
+            await expect(this.page.getByText('Please select date for appointment')).toBeVisible();
+        }
+        else if(Incomplete === "dentist"){
+            await expect(this.page.getByText('Please select dentist')).toBeVisible();
+        }
+        else if(Incomplete === "all"){
+            await expect(this.page.getByText('Please select dentist')).toBeVisible();
+        }
+      
         await expect(this.page.getByRole('button', { name: '👍 OK!' })).toBeVisible();
         await this.page.getByRole('button', { name: '👍 OK!' }).click();
     }
 
-    async VerifyCompleteBooking() {
+    async VerifyFailBooking( type : "exist" | "past" | "invalid" | "morethan1") {
         await expect(this.page.locator('div').filter({ hasText: '!' }).nth(2)).toBeVisible();
-        await expect(this.page.getByLabel('Complete')).toBeVisible();
-        await expect(this.page.getByRole('heading', { name: 'Complete' })).toBeVisible();
-        await expect(this.page.getByText('Your appointment has been booked')).toBeVisible();
+        await expect(this.page.getByRole('heading', { name: 'Failed' })).toBeVisible();
+        if(type === "past"){
+            await expect(this.page.getByText('Cannot book appointment in the past')).toBeVisible();
+        }
+        else if(type === "invalid"){
+            await expect(this.page.getByText('Failed to add appointment')).toBeVisible();
+        }
+        else if(type === "exist"){
+            await expect(this.page.getByText('Appointment date and dentist already exists')).toBeVisible();
+        }
         await expect(this.page.getByRole('button', { name: '👍 OK!' })).toBeVisible();
         await this.page.getByRole('button', { name: '👍 OK!' }).click();
     }
+
+
+
+    async VerifyCompleteBooking() {
+        await expect(this.page.locator('div').filter({ hasText: '!' }).nth(2)).toBeVisible();
+        await expect(this.page.getByRole('heading', { name: 'Successfully' })).toBeVisible();
+        await expect(this.page.getByText('Appointment booked successfully')).toBeVisible();
+        await expect(this.page.getByRole('button', { name: '👍 OK!' })).toBeVisible();
+        await this.page.getByRole('button', { name: '👍 OK!' }).click();
+    }
+
+    async VerifyAppointmentPage() {
+        await expect(this.page.getByRole('heading', { name: 'Patient Appointments' })).toBeVisible();
+        await expect(this.page.getByText('Patient : test01 case01Dentist : Doctor Emma ConsidineAppointment Date : 27 /')).toBeVisible();
+        await expect(this.page.locator('.text-lg').first()).toBeVisible();
+        await expect(this.page.getByText('Patient : test01 case01')).toBeVisible();
+        await expect(this.page.getByText('Dentist : Doctor Emma')).toBeVisible();
+        await expect(this.page.getByText('Appointment Date : 27 / 07 /')).toBeVisible();
+        await expect(this.page.getByRole('button', { name: 'Cancel' })).toBeVisible();
+        await expect(this.page.getByRole('button', { name: 'Edit Appointment' })).toBeVisible();
+    };
+
 }
--- testcase/tests/booking.test.ts
+++ testcase/tests/booking.test.ts
@@ -11,7 +11,7 @@ test.describe('addBookin', () => {
         await baseTest.login('test01@gmail.com' , "Test01");
         await baseTest.navigateTobooking();
         await baseTest.verifyBookingPage();
-        await baseTest.filldatabooking("Emma Considine" , "afternoon");
+        await baseTest.filldatabooking("Emma Considine" , "morning" , '27' , 6 , '2025' , false);
         await baseTest.VerifyCompleteBooking();
     });
 
@@ -20,7 +20,108 @@ test.describe('addBookin', () => {
         await baseTest.navigateToSignIn();
         await baseTest.login('test01@gmail.com' , "Test01");
         await baseTest.navigateTobooking();
-        await baseTest.verifyBookingPage();
-        await baseTest.filldatabooking("Emma Considine" , "morning");
+        await baseTest.filldatabooking("Emma Considine" , "morning", '27' , 6 , '2023' , true);
+        await baseTest.VerifyFailBooking("past");
     })
+
+    test('patient invalid date booking (data invalid)', async ({ page }) => {
+        const baseTest = new BaseTest(page);
+        await baseTest.navigateToSignIn();
+        await baseTest.login('test01@gmail.com' , "Test01");
+        await baseTest.navigateTobooking();
+        await baseTest.filldatabooking("Emma Considine" , "morning", '99' , 99 , '9999' , true);
+        await baseTest.VerifyFailBooking("invalid");
+    })
+
+    test('patient incomplete booking (Dentist)', async ({ page }) => {
+        const baseTest = new BaseTest(page);
+        await baseTest.navigateToSignIn();
+        await baseTest.login('test01@gmail.com' , "Test01");
+        await baseTest.navigateTobooking();
+        await baseTest.filldatabooking(null , "morning", '27' , 6 , '2025' , true);
+        await baseTest.VerifyIncompleteBooking("dentist");
+    })
+
+    
+    test('patient incomplete booking (date)', async ({ page }) => {
+        const baseTest = new BaseTest(page);
+        await baseTest.navigateToSignIn();
+        await baseTest.login('test01@gmail.com' , "Test01");
+        await baseTest.navigateTobooking();
+        await baseTest.filldatabooking("Emma Considine" ,"morning", null , null , null , true);
+        await baseTest.VerifyIncompleteBooking("date")
+    })
+
+    
+    test('patient incomplete booking (time)', async ({ page }) => {
+        const baseTest = new BaseTest(page);
+        await baseTest.navigateToSignIn();
+        await baseTest.login('test01@gmail.com' , "Test01");
+        await baseTest.navigateTobooking();
+        await baseTest.filldatabooking("Emma Considine" , null, '27' , 6 , '2025' , true);
+        await baseTest.VerifyIncompleteBooking("time")
+    })
+
+    
+    test('patient incomplete booking (all)', async ({ page }) => {
+        const baseTest = new BaseTest(page);
+        await baseTest.navigateToSignIn();
+        await baseTest.login('test01@gmail.com' , "Test01");
+        await baseTest.navigateTobooking();
+        await baseTest.filldatabooking(null , null,null ,null , null , true);
+        await baseTest.VerifyIncompleteBooking("all")
+    })
+
+    //patien booking dentist and date that already exists
+    test('patient booking dentist and date that already exists', async ({ page }) => {
+        const baseTest = new BaseTest(page);
+        await baseTest.navigateToSignIn();
+        await baseTest.login('test02@gmail.com' , "Test01");
+        await baseTest.navigateTobooking();
+        await baseTest.filldatabooking("Emma Considine" , "morning", '27' , 6 , '2025' , false);
+        await baseTest.VerifyFailBooking("exist");
+    })
+
+    //patien can not booking more than 1 appointment 
+    test('patient booking more than 1 appointment', async ({ page }) => {
+        const baseTest = new BaseTest(page);
+        await baseTest.navigateToSignIn();
+        await baseTest.login('test01@gmail.com' , "Test01");
+        await baseTest.navigateTobooking();
+        await baseTest.filldatabooking("Emma Considine" , "morning", '27' , 7 , '2025' , false);
+        await baseTest.VerifyFailBooking("morethan1");
+    });
+
+    //admin can not booking
+    test('admin can not booking', async ({ page }) => {
+        const baseTest = new BaseTest(page);
+        await baseTest.navigateToSignIn();
+        await baseTest.login('admin@gmail.com' , '123456')
+        await baseTest.page.goto('https://frontendsw-mtl.vercel.app/makeappointment');
+        await baseTest.page.waitForURL('https://frontendsw-mtl.vercel.app/');
+        expect(baseTest.page.url()).toBe('https://frontendsw-mtl.vercel.app/');
+    });
+
+   // dentist can not booking
+    test('dentist can not booking', async ({ page }) => {
+        const baseTest = new BaseTest(page);
+        await baseTest.navigateToSignIn();
+        await baseTest.login('dentist01@gmail.com' , 'Test01')
+        await baseTest.page.goto('https://frontendsw-mtl.vercel.app/makeappointment');
+        await baseTest.page.waitForURL('https://frontendsw-mtl.vercel.app/');
+        expect(baseTest.page.url()).toBe('https://frontendsw-mtl.vercel.app/');
+    });
+
+
+}); 
+
+test.describe('viewBooking', () => {
+
+    test('check element', async ({ page }) => {
+        const baseTest = new BaseTest(page);
+        await baseTest.navigateToSignIn();
+        await baseTest.login('test01@gmail.com' , "Test01");
+        await baseTest.navigateToappointment();
+        await baseTest.VerifyAppointmentPage();
+    });
 });
